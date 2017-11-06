4c86a2d8ed24142daba06701db42b8ef3f698d29
John Furr <jfurr@Johns-MacBook-Pro.local>
2017-11-05 19:31:15 -0500

    hold

--- goperf.go
+++ goperf.go
@@ -1,31 +1,39 @@
 package main
 
 import (
-    "fmt"
-    "net/http"
+	"fmt"
+	"net/http"
+	"time"
 )
 
 func main() {
-    url := "https://qa.teaquinox.com"
-    //url := "https://www.google.com"
+	url := "https://qa.teaquinox.com"
+	//url := "https://teaquinox.com"
+	//Create the basic request
 
-    client := &http.Client{
-        //CheckRedirect: redirectPolicyFunc,
-    }
 
-    //Create the basic request
-    req, _ := http.NewRequest("GET", url, nil)
-    req.Header.Add("user-agent", "Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Mobile Safari/537.36")
-    
-    for i:=0; i<10; i++ {
-        resp, err := client.Do(req)
-        fmt.Println("resp: ", resp)
-        fmt.Println("err: ", err)
-    }
+	for i := 0; i < 100; i++ {
+		go run(url, i)
+	}
+	run(url, 11)
 }
 
+func run(url string, index int) {
+	client := &http.Client{}
 
-func get_url(count int) (time float64) {
-    
-    return (1.234)
+	req, _ := http.NewRequest("GET", url, nil)
+	req.Header.Add("user-agent", "Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Mobile Safari/537.36")
+	//url := "https://www.google.com"
+
+
+	iterations := 1000
+	start := time.Now()
+	for i := 0; i < iterations; i++ {
+		client.Do(req)
+		fmt.Println(index)
+	}
+	end := time.Now()
+	total := end.Sub(start)
+	fmt.Println("Total: ", total)
+	fmt.Println("Avg: ", total/time.Duration(iterations))
 }
