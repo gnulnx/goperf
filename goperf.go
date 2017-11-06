7fb096a3ba211e1aa3a51893acaf4a34ff384ae3
John Furr <jfurr@Johns-MacBook-Pro.local>
2017-11-05 20:38:50 -0500

    Command line args introduced

--- goperf.go
+++ goperf.go
@@ -4,33 +4,35 @@ import (
 	"fmt"
 	"net/http"
 	"time"
+    "flag"
 )
 
 func main() {
-	url := "https://qa.teaquinox.com"
-	//url := "https://teaquinox.com"
-	//Create the basic request
+    threads := flag.Int("threads", 10, "Number of Threads")
+    url := flag.String("url", "https://qa.teaquinox.com", "url to test")
+    iterations := flag.Int("iter", 1000, "Iterations per thread")
+    flag.Parse()
 
+    fmt.Println("Running again url:", *url)
+    fmt.Println("threads: ", *threads)
 
-	for i := 0; i < 100; i++ {
-		go run(url, i)
+	for i := 0; i < *threads-1; i++ {
+		go run(*url, i, *iterations)
 	}
-	run(url, 11)
+	run(*url, *threads, *iterations)
 }
 
-func run(url string, index int) {
+func run(url string, index int, iterations int) {
 	client := &http.Client{}
 
 	req, _ := http.NewRequest("GET", url, nil)
 	req.Header.Add("user-agent", "Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Mobile Safari/537.36")
-	//url := "https://www.google.com"
-
-
-	iterations := 1000
 	start := time.Now()
 	for i := 0; i < iterations; i++ {
 		client.Do(req)
-		fmt.Println(index)
+        if i % 3 == 0 {
+            fmt.Println("Thread: ", index, " iteration: ", i)
+        }
 	}
 	end := time.Now()
 	total := end.Sub(start)
