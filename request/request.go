65c5679f07f63ed66ac3a815afc6ad683d7ef355
John Furr <jfurr@Johns-MacBook-Pro.local>
2017-11-06 18:42:55 -0500

    better comments

--- request/request.go
+++ request/request.go
@@ -2,52 +2,61 @@ package request
 
 import (
 	"fmt"
-	"time"
 	"net/http"
+	"time"
 )
 
-/* Results for a perf thread */
+/*
+   Structure to hold the results from a 'go Run(...)' method call
+*/
 type Result struct {
-    Total   time.Duration
-    Average time.Duration
-    Channel int
+	Total   time.Duration
+	Average time.Duration
+	Channel int
 }
 
 //display method for Results
 func (r *Result) Display() {
-    fmt.Println("Channel(", r.Channel, ") Total(", r.Total, ") Average(", r.Average, ")")
+	fmt.Println("Channel(", r.Channel, ") Total(", r.Total, ") Average(", r.Average, ")")
 }
 
-// This is the input structure for a perf thread
+/*
+   Structure to hold the input variables to 'go Run(...)' method call
+*/
 type Input struct {
-    Url        string
-    Threads    int
-    Iterations int
-    Output     int
-    Index      int // Also the channel number
+	Url        string
+	Threads    int
+	Iterations int
+	Output     int
+	Index      int // Also the channel number
 }
 
-// This is the main url perf testing method
+/*
+   Run the input parameters defined in Input struct.
+   channel 'done' expects a Result object
+*/
 func (input Input) Run(done chan Result) {
-    client := &http.Client{}
+	client := &http.Client{}
 
-    req, _ := http.NewRequest("GET", input.Url, nil)
-    req.Header.Add("user-agent", "Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Mobile Safari/537.36")
-    start := time.Now()
-    for i := 0; i < input.Iterations; i++ {
-        client.Do(req)
-        if i % input.Output == 0 {
-            fmt.Println("Thread: ", input.Index, " iteration: ", i)
-        }
-    }
-    end := time.Now()
-    total := end.Sub(start)
-    average := total / time.Duration(input.Iterations)
+	req, _ := http.NewRequest("GET", input.Url, nil)
+	req.Header.Add("user-agent", "Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Mobile Safari/537.36")
 
-    // Send results on done channel
-    done <- Result{
-        Total:   total,
-        Average: average,
-        Channel: input.Index,
-    }
+	start := time.Now()
+
+	for i := 0; i < input.Iterations; i++ {
+		client.Do(req)
+		if i%input.Output == 0 {
+			fmt.Println("Thread: ", input.Index, " iteration: ", i)
+		}
+	}
+	end := time.Now()
+	total := end.Sub(start)
+	average := total / time.Duration(input.Iterations)
+
+	// Send results on done channel
+	done <- Result{
+		Total:   total,
+		Average: average,
+		Channel: input.Index,
+	}
 }
